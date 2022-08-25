6bfb96ef1dad05e6f1f09f414e9a0498e4ded325
Aman Kumar <kumar.aman444@gmail.com>
2022-08-25 16:38:46 +0530

    first commit

---
+++ fields.js
@@ -0,0 +1,15 @@
+// console.log(__dirname)
+// setInterval(()=>{
+//     console.log("Hello!!")
+// },1000)
+
+const john = 'john';
+const peter = 'peter';
+
+const sayHi = (name) =>{
+    console.log(`Hi ${name}`)
+}
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
